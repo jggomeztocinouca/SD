909d965b6a3ff570b054406dbbf512613f31e70f
jggomeztocinouca <jesus.gomeztocino@alum.uca.es>
2023-04-12 16:52:29 +0200

    Comienzo de 0.

--- Practica2/Ejercicio1.py
+++
@@ -1,4 +0,0 @@
-# Implemente un programa cliente/servidor que comunique dos procesos con los protocolos TCP y UDP.
-# ■ Explique las diferencias existentes entre ambas implementaciones.
-# ■ ¿Qué ocurre si el cliente se inicia antes que el servidor en ambos casos (TCP y UDP)?
-
