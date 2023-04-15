037515aa3c2881a0d3242fdadb20a77d2a9c4970
jggomeztocinouca <90068817+jggomeztocinouca@users.noreply.github.com>
2023-04-15 13:53:03 +0200

    Delete Practica1 directory

--- Practica1/Ejercicios/ejercicio13.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print("Listado de directorios:")
-# El fichero '/etc/passwd, que contiene información de los usuarios del sistema, incluyendo su directorio de inicio.
-with open('/etc/passwd') as file:
-	for line in file:
-	    home_dir = line.split(':')[5]
-	    print(home_dir)
-
